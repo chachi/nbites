b7a5b27c756b7d1837eec778cfe2a895aca6c777
Jack Morrison <jgmorris@bowdoin.edu>
2011-05-30 23:07:53 -0400

    Removed verbose untars, fixed usage echo, added looping question, removed apt-get verbosity.

--- util/scripts/linux_setup.sh
+++ util/scripts/linux_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-	echo "usage: ./linux_setup_x64.sh [ naoqi-version ]"
+	echo "usage: ./linux_setup.sh [ naoqi-version ]"
 	exit 1
 fi
 
@@ -11,16 +11,25 @@ pressing TAB!"
 PACKAGES="build-essential cmake git-core sun-java6-jdk \
 python2.6-dev emacs cmake-curses-gui ccache curl aptitude"
 
-echo "Are you on 64-bit linux?(y/n)"
-read IS64BIT
+while [[ $IS64BIT != 'n' && $IS64BIT != 'y' ]]; do
+    echo "Are you on 64-bit linux? [y/n]"
+    read IS64BIT
+done
 
+# Read in username now, so no further interaction is needed
+echo ""
+echo "What's your Bowdoin username?"
+read USER_NAME
+
+# Add on 64-bit GCC compatibility if necessary
 if [ $IS64BIT == 'y' ]; then
   PACKAGES="$PACKAGES g++-4.4-multilib"
 fi
 
-sudo add-apt-repository ppa:sun-java-community-team/sun-java6
-sudo apt-get update
-sudo apt-get install $PACKAGES
+# Install new packages
+sudo add-apt-repository ppa:sun-java-community-team/sun-java6 > /dev/null
+sudo apt-get -qq update
+sudo apt-get -qq install -y $PACKAGES
 sudo update-java-alternatives -s java-6-sun
 
 echo "Downloading and unpacking NBites files"
@@ -37,10 +46,6 @@ naoqi_local=$lib_dir/naoqi-sdk-$naoqi_version-linux
 ext=ext-nbites-linux.tar.gz
 ext_robocup=$robocup/software/$ext
 
-echo ""
-echo "What's your Bowdoin username?"
-read USER_NAME
-
 echo "Downloading NaoQi"
 mkdir -p $lib_dir
 rsync -v $USER_NAME@$naoqi_robocup $lib_dir/
@@ -48,7 +53,7 @@ rsync -v $USER_NAME@$naoqi_robocup $lib_dir/
 echo "Unpacking NaoQi"
 
 pushd $lib_dir
-tar -xvzf $naoqi
+tar -xzf $naoqi
 rm $naoqi
 popd
 
@@ -63,7 +68,7 @@ rsync -v $USER@$ctc_robocup $naoqi_local/
 echo "Unpacking the CTC"
 
 pushd $naoqi_local
-tar -xvzf $ctc
+tar -xzf $ctc
 rm $ctc
 popd
 fi
@@ -74,7 +79,7 @@ rsync -v $USER_NAME@$ext_robocup $nbites_dir/
 
 echo "Unpacking ext"
 pushd $nbites_dir
-tar -xvzf $ext
+tar -xzf $ext
 rm $ext
 popd
 
