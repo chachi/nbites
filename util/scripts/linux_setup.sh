fc8c8dbad5115991f41d68d8b3815b8479f1b636
Jack Morrison <jgmorris@bowdoin.edu>
2011-06-01 00:03:35 -0400

    Added ant package install and removed aptitude install.

--- util/scripts/linux_setup.sh
+++ util/scripts/linux_setup.sh
@@ -9,7 +9,7 @@ echo "Downloading awesome free stuff! Also accept the Sun Java licence by \
 pressing TAB!"
 
 PACKAGES="build-essential cmake git-core sun-java6-jdk \
-python2.6-dev emacs cmake-curses-gui ccache curl aptitude"
+python2.6-dev emacs cmake-curses-gui ccache curl ant"
 
 while [[ $IS64BIT != 'n' && $IS64BIT != 'y' ]]; do
     echo "Are you on 64-bit linux? [y/n]"
