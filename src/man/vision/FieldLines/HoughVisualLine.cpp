e1a03459c8afccfc0c9149787b34d63a3da70b90
Jack Morrison <jackmorrison1@gmail.com>
2012-06-10 11:57:16 -0400

    Calculates point on HoughVisualLine and slope.

--- src/man/vision/FieldLines/HoughVisualLine.cpp
+++ src/man/vision/FieldLines/HoughVisualLine.cpp
@@ -1,6 +1,72 @@
 #include "HoughVisualLine.h"
-using namespace std;
 #include <iostream>
+#include "Utility.h"
+
+using namespace std;
+
+HoughVisualLine::HoughVisualLine(const HoughLine& a, const HoughLine& b) :
+    mLines(a, b)
+{
+    calculateLineParameters();
+}
+
+void HoughVisualLine::calculateLinePoint()
+{
+    if (mLines.first.intersects(mLines.second, mPoint)) {
+        mPoint.x += IMAGE_WIDTH/2;
+        mPoint.y += IMAGE_HEIGHT/2;
+    } else {
+        HoughLine& hl_1 = mLines.first;
+        HoughLine& hl_2 = mLines.second;
+        point<double> onHL_1 = hl_1.getOriginPoint();
+
+        point<double> onHL_2_1 = hl_2.getOriginPoint();
+
+        // Get a point a distance from the first
+        point<double> onHL_2_2(onHL_2_1.x + hl_2.getSinT() * 5,
+                               onHL_2_1.x - hl_2.getCosT() * 5);
+
+        // If they're parallel, then just get the point halfway
+        // between the two lines
+        point<int> onHL_2_close = Utility::getClosestPointOnLine(onHL_1,
+                                                                  onHL_2_1,
+                                                                  onHL_2_2);
+        mPoint = point<double>((onHL_1.x + onHL_2_close.x)/2,
+                               (onHL_1.y + onHL_2_close.y)/2);
+    }
+}
+
+void HoughVisualLine::calculateSlope()
+{
+    if (mLines.first.intersects(mLines.second, mPoint)) {
+        // Angle bisector of two lines:
+        // Calculation from
+// http://demonstrations.wolfram.com/AngleBisectorsOfTwoIntersectingLines/
+        double dy_1 = -mLines.first.getCosT();
+        double dx_1 =  mLines.first.getSinT();
+
+        double dy_2 = -mLines.second.getCosT();
+        double dx_2 =  mLines.second.getSinT();
+
+        double m_1 = dy_1/dx_1;
+        double m_2 = dy_2/dx_2;
+
+        mDy = m_1*m_2 - 1 + sqrt((m_1*m_1 + 1)*(m_2*m_2 + 1));
+        mDx = m_1 + m_2;
+
+    } else {
+        // If they're parallel, then just use the slope of one of the
+        // lines
+        mDy = -mLines.first.getCosT();
+        mDx = mLines.first.getSinT();
+    }
+}
+
+void HoughVisualLine::calculateLineParameters()
+{
+    calculateLinePoint();
+    calculateSlope();
+}
 
 bool HoughVisualLine::intersects(const HoughVisualLine& other,
                                  point<int>& out) const
@@ -17,4 +83,19 @@ bool HoughVisualLine::intersects(const HoughVisualLine& other,
         out.y = (a.y + b.y + c.y + d.y)/4;
     }
     return allIntersecting;
+}
+
+double HoughVisualLine::getDy() const
+{
+    return mDy;
+}
+
+double HoughVisualLine::getDx() const
+{
+    return mDx;
+}
+
+point<int> HoughVisualLine::getPointOnLine() const
+{
+    return mPoint;
 }
--- src/man/vision/FieldLines/HoughVisualLine.h
+++ src/man/vision/FieldLines/HoughVisualLine.h
@@ -6,17 +6,27 @@
 class HoughVisualLine
 {
 public:
-    HoughVisualLine(const HoughLine& a, const HoughLine& b) :
-        mLines(a, b)
-    {}
+    HoughVisualLine(const HoughLine& a, const HoughLine& b);
 
     virtual ~HoughVisualLine() {}
 
     std::pair<HoughLine, HoughLine> getHoughLines() const { return mLines; }
     bool intersects(const HoughVisualLine& other, point<int>& out) const;
 
+    double getDx() const;
+    double getDy() const;
+    point<int> getPointOnLine() const;
+
+
 private:
+    void calculateLineParameters();
+    void calculateLinePoint();
+    void calculateSlope();
+
     std::pair<HoughLine, HoughLine> mLines;
+
+    double mDx, mDy;
+    point<int> mPoint;
 };
 
 #endif /* _HOUGHVISUALLINE_H_ */
